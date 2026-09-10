e12bdc9b93738ab3905dac3cdb03ffd99106897a
lmdoyle <leodoyle05@gmail.com>
2026-09-10 09:27:17 +0200

    Deploy: lighter neutral overlay on photo share cards (drop green tint)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2880904295" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2772111687" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
